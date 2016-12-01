c6faaa29396d7732d7e300d0ca399dca80936d52
Promasu <admin.noethlich@hgw-iserv.de>
2016-12-01 17:45:29 +0100

    Added data for testing, interface for data and errorhandling

--- tool.py
+++ tool.py
@@ -4,7 +4,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-import xml.etree.ElementTree as et
+import xml.etree.ElementTree as eT
 
 class tool(QMainWindow):
     def __init__(self):
@@ -13,19 +13,30 @@ class tool(QMainWindow):
         self.initUI()
 
     def initUI(self):
-
         exitAction = QAction('&Schließen', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.triggered.connect(qApp.quit)
 
         openAction = QAction('&Öffnen', self)
         openAction.setShortcut('Ctrl+O')
-        filename = openAction.triggered.connect(lambda: self.importHandler(frameLeft))
+        openAction.triggered.connect(lambda: self.importHandler(frameLeft, frameRight))
+
+        addAction = QAction('&Eintrag anlegen', self)
+        addAction.setShortcut('Ctrl+N')
+        addAction.triggered.connect(lambda: self.addHandler(frameLeft))
+
+        editmodeAction = QAction('&Bearbeitungsmodus', self)
+        editmodeAction.setShortcut('Ctrl+E')
+        editmodeAction.triggered.connect(lambda: self.editmodeHandler(frameRight))
 
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&Datei')
         fileMenu.addAction(openAction)
+        fileMenu.addAction(addAction)
         fileMenu.addAction(exitAction)
+
+        editMenu = menubar.addMenu('&Bearbeiten')
+        editMenu.addAction(editmodeAction)
 
         screenbox = QWidget()
 
@@ -34,9 +45,15 @@ class tool(QMainWindow):
 
         frameLeft = QListWidget()
         frameLeft.setMaximumWidth(frameLeft.sizeHintForColumn(0))
+        frameLeft.currentItemChanged.connect(lambda: self.displayInfo(frameLeft, frameRight))
+        frameLeft.connect(frameLeft, QSignalMapper("itemDoubleClicked(QListWidgetItem*)"))
+        frameLeft.connect(frameLeft, QSignalMapper("itemDoubleClicked(QListWidgetItem*)", lambda: self.testBox()))
 
-        frameRight = QFrame(self)
-        frameRight.setFrameShape(QFrame.StyledPanel)
+
+        frameRight = QLabel()
+        frameRight.setTextFormat(Qt.RichText)
+        frameRight.setTextInteractionFlags(Qt.TextBrowserInteraction)
+        frameRight.setOpenExternalLinks(True)
 
         screenSplitter = QSplitter(Qt.Horizontal)
         screenSplitter.addWidget(frameLeft)
@@ -49,18 +66,69 @@ class tool(QMainWindow):
         self.setWindowTitle('Promasu Manager')
         self.show()
 
-    def importHandler(self, listView):
+    def importHandler(self, listView, labelView):
         fileDialog = QFileDialog()
         fileDialog.setFileMode(QFileDialog.ExistingFile)
         filename = fileDialog.getOpenFileName(self, 'Promasu Datenbank öffnen', '.', 'Promasu Projectdatabase (*.pmdx)')
-        tree = et.parse(filename[0])
+        try:
+            tree = eT.parse(filename[0])
+            root = tree.getroot()
+            listView.clear()
+            for child in root:
+                listView.addItem(child[0].text)
+            listView.setMaximumWidth(listView.sizeHintForColumn(0) + 10)
+            listView.setMinimumWidth(listView.sizeHintForColumn(0) + 10)
+            labelView.setText(filename[0])
+            self.filename = str(filename[0])
+        except eT.ParseError:
+            msgBox = QMessageBox()
+            msgBox.setText("Die Datei hat kein gültiges Format.")
+            msgBox.setWindowTitle("Promasu Manager Warnung")
+            msgBox.exec()
+            return False
+        except FileNotFoundError:
+            msgBox = QMessageBox()
+            msgBox.setText("Die Datei wurde nicht gefunden.")
+            msgBox.setWindowTitle("Promasu Manager Warnung")
+            return False
+        except IndexError:
+            msgBox = QMessageBox()
+            msgBox.setText("Die Datei ist nicht im richtigen Format.")
+            msgBox.setWindowTitle("Promasu Manager Warnung")
+            return False
+        except PermissionError:
+            msgBox = QMessageBox()
+            msgBox.setText("Sie besitzen nichtdie Berechtigung um diese Datei zu öffnen.")
+            msgBox.setWindowTitle("Promasu Manager Warnung")
+            return False
+        return True
+
+    def displayInfo(self, listView, labelView):
+        tree = eT.parse(self.filename)
         root = tree.getroot()
-        listView.clear()
-        for child in root:
-            listView.addItem(child[0].text)
-        listView.setMaximumWidth(listView.sizeHintForColumn(0))
-        self.setWindowTitle("Promasu Manager - " + filename[0])
-        return filename[0]
+        itemNumber = listView.currentRow()
+        saveString = "Speicherort: <a href=\"file:///" + root[itemNumber][1].text + "\">" + root[itemNumber][1].text + "</a><br>"
+        buildString = "Build: <a href=\"file:///" + root[itemNumber][2].text + "\">" + root[itemNumber][2].text + "</a><br>"
+        gitString = "Git: <a href=\"" + root[itemNumber][3].text + "\">" + root[itemNumber][3].text + "</a><br>"
+        docString = "Dokumentation: <a href=\"" + root[itemNumber][4].text + "\">" + root[itemNumber][4].text + "</a><br>"
+        designString = "Designs: <a href=\"file:///" + root[itemNumber][5].text + "\">" + root[itemNumber][5].text + "</a><br>"
+        imageString = "Bilder: <a href=\"file:///" + root[itemNumber][6].text + "\">" + root[itemNumber][6].text + "</a><br>"
+        langString = "Sprache: " + root[itemNumber][7].text + "<br>"
+        versionString = "Version: " + root[itemNumber][8].text
+        labelView.setText(saveString+buildString+gitString+docString+designString+imageString+langString+versionString)
+        return True
+
+    def addHandler(self, listView):
+        pass
+
+    def editmodeHandler(self, labelView):
+        pass
+
+    def testBox(self):
+        msgBox = QMessageBox()
+        msgBox.setText("Es klappt.")
+        msgBox.setWindowTitle("Promasu Manager")
+        return True
 
     def onChanged(self, text):
         self.lbl.setText(text)
